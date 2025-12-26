0edcccce45fcdfd0f9a726480fc1e3cdc46b0ee4
znlumins <daffaahmadalattas@gmail.com>
2025-12-27 04:06:29 +0700

    v0.1minor

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "kalender timelini hmpsti",
+  title: "kalender timeline hmpsti",
   description: "Aplikasi kalender kegiatan HMPSTI UB",
 };
 
